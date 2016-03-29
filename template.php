c9681abb5cf5e4dd40bf8ad1e7b68507c1b1aa27
Jesse Boyd <jesse.boyd@industry.gov.au>
2016-03-29 17:23:17 +1100

    Added button styling and file table format.

--- template.php
+++ template.php
@@ -28,4 +28,74 @@ function nathers_2016_form_search_block_form_alter(&$form, &$form_state, $form_i
     // Alternative (HTML5) placeholder attribute instead of using the javascript
     $form['search_block_form']['#attributes']['placeholder'] = t('Search');
 } 
+
+function nathers_2016_file_formatter_table($variables) {
+    $list = array(
+        'items' => array(),
+        'title' => '',
+        'type' => 'ul',
+        'attributes' => array(
+            'class' => 'buttons',
+        ),
+    );
+    foreach ($variables['items'] as $delta => $item) {
+        $list['items'][] = theme('file_link', array('file' => (object) $item));
+    }
+    asort($list);
+    return empty($list) ? '' : theme('item_list', $list);
+}
+
+function nathers_2016_file_link($variables) {
+    $file = $variables['file'];
+    $icon_directory = $variables['icon_directory'];
+
+    $url = file_create_url($file->uri);
+
+    // Human-readable names, for use as text-alternatives to icons.
+    $mime_name = array(
+        'application/msword' => t('Microsoft Office document icon'),
+        'application/vnd.ms-excel' => t('Office spreadsheet icon'),
+        'application/vnd.ms-powerpoint' => t('Office presentation icon'),
+        'application/pdf' => t('PDF icon'),
+        'video/quicktime' => t('Movie icon'),
+        'audio/mpeg' => t('Audio icon'),
+        'audio/wav' => t('Audio icon'),
+        'image/jpeg' => t('Image icon'),
+        'image/png' => t('Image icon'),
+        'image/gif' => t('Image icon'),
+        'application/zip' => t('Package icon'),
+        'text/html' => t('HTML icon'),
+        'text/plain' => t('Plain text icon'),
+        'application/octet-stream' => t('Binary Data'),
+    );
+
+    $mimetype = file_get_mimetype($file->uri);
+
+    /*$icon = theme('file_icon', array(
+        'file' => $file,
+        'icon_directory' => $icon_directory,
+        'alt' => !empty($mime_name[$mimetype]) ? $mime_name[$mimetype] : t('File'),
+    ));*/
+
+    // Set options as per anchor format described at
+    // http://microformats.org/wiki/file-format-examples
+    $options = array(
+        'attributes' => array(
+        'type' => $file->filemime . '; length=' . $file->filesize,
+        ),
+    );
+    // Get the file extension
+    $file->extension = strtoupper(pathinfo($file->filename, PATHINFO_EXTENSION));
+
+    // Use the description as the link text if available.
+    dsm($file);
+    if (empty($file->description)) {
+        $link_text = $file->filename . ' (' . $file->extension . ', ' . format_size($file->filesize) . ')';
+    } else {
+        $link_text = $file->description . ' (' . $file->extension . ', ' . format_size($file->filesize) . ')';
+        $options['attributes']['title'] = check_plain($file->filename);
+    }
+
+    return l($link_text, $url, $options);
+}
 ?>
